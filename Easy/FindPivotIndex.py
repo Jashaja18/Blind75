a849c565eaeadfbab80592fb23062b26fc500521
Jashanpreet Jandu <jashanjandu23@gmail.com>
2022-11-21 13:23:44 -0800

    Day 2 of the Blind 75

--- Easy/FindPivotIndex.py
+++ Easy/FindPivotIndex.py
@@ -8,17 +8,3 @@ class Solution:
                 return i
             leftSum += nums[i]
         return -1
-
-        # variables for the left and rightmost edges of the array
-        sum_left = nums[0]
-        sum_right = nums[-1]
-
-        for i in range(len(nums)):
-            if nums == 1:
-                sum_left[i] += sum_left[i - 1]
-            if nums == sum_right:
-                sum_right[i] += sum_right[i + 1]
-            if nums == 0:
-                sum_left[i] == sum_right[i]
-            else:
-                return -1
